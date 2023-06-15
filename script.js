ff0f5bc9a61a329deb87d2452a074ec1c5f8e0f4
Zar117 <70678148+Zar117@users.noreply.github.com>
2023-06-14 21:40:20 -0400

    TinyBird Skills

--- script.js
+++ script.js
@@ -22,25 +22,44 @@ function showSection(id) {
 showSection('education'); // Show the "Education" section by default
 
 window.onload = function() {
-    const API_URL = "https://api.us-east.tinybird.co/v0/pipes/work_experience_pipe_6072.json?token=p.eyJ1IjogImJlMjI0MjJiLWMxZDMtNDM3OC05OTY3LTBiZmJlZTI4ZDM0ZSIsICJpZCI6ICIzMDUxYTc1OC1kNTk0LTQ3YTUtYjY3Mi1iNmFjYmI5Y2I0ZTgifQ.-8_l7AY-hvEKWkhGpJ29SYd1-SSGiiTWGcMTszsOrRI";
-    fetch(API_URL)
+    const WORK_API_URL = "https://api.us-east.tinybird.co/v0/pipes/work_experience_pipe_6072.json?token=p.eyJ1IjogImJlMjI0MjJiLWMxZDMtNDM3OC05OTY3LTBiZmJlZTI4ZDM0ZSIsICJpZCI6ICIzMDUxYTc1OC1kNTk0LTQ3YTUtYjY3Mi1iNmFjYmI5Y2I0ZTgifQ.-8_l7AY-hvEKWkhGpJ29SYd1-SSGiiTWGcMTszsOrRI";
+    const SKILLS_API_URL = "https://api.us-east.tinybird.co/v0/pipes/skills_pipe_0872.json?token=p.eyJ1IjogImJlMjI0MjJiLWMxZDMtNDM3OC05OTY3LTBiZmJlZTI4ZDM0ZSIsICJpZCI6ICIwMWM1YjY1ZS1jNmQ4LTQwNjMtOWQ0Yi1lZmJlZmIzNjdkNjIifQ.kEVNkZUnLUuIXC--6cIDJvKYyWQ_ae7CTBoF_3-fVeo";
+
+    // Work Experience
+    fetch(WORK_API_URL)
         .then(response => response.json())
         .then(data => {
             const workExperienceList = document.getElementById('workExperienceList');
-            
-            // Sort the data array by the startDate field in descending order
-            data.data.sort((a, b) => new Date(b.startDate) - new Date(a.startDate));
-
             data.data.forEach(item => {
                 const li = document.createElement('li');
                 li.innerHTML = `
                     <strong>${item.Company}: ${item.Position}</strong><br>
-                    <strong>${item.Start_Date} – ${item.End_Date}</strong><br>
-                    <i>${item.Location}</i><br>
+                    <strong> ${item.Start_Date} – ${item.End_Date} </strong> <br>
+                     <i> ${item.Location} </i> <br>
                     ${item.Responsibilities.replace(/\n/g, '<br>')}
                     <br>
                 `;
                 workExperienceList.appendChild(li);
             });
+        });
+
+    // Skills
+    fetch(SKILLS_API_URL)
+        .then(response => response.json())
+        .then(data => {
+            const skillsList = document.getElementById('skills').getElementsByTagName('ul')[0];
+            skillsList.innerHTML = ''; // clear out the existing list items
+            let categories = {};
+            data.data.forEach(item => {
+                if(!categories[item.Category]) {
+                    categories[item.Category] = [];
+                }
+                categories[item.Category].push(item.Skill);
+            });
+            for(const category in categories) {
+                const li = document.createElement('li');
+                li.innerHTML = `<strong>${category}:</strong> ${categories[category].join(', ')}`;
+                skillsList.appendChild(li);
+            }
         });
 };
