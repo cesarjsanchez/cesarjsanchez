323b43f89c228896e8a445c2160eb9405cebaf04
Cesar Sanchez <135764510+cesarjsanchez@users.noreply.github.com>
2023-06-14 21:21:15 -0400

    TinyBird work Exp

--- script.js
+++ script.js
@@ -20,3 +20,27 @@ function showSection(id) {
 }
 
 showSection('education'); // Show the "Education" section by default
+
+window.onload = function() {
+    const API_URL = "https://api.us-east.tinybird.co/v0/pipes/work_experience_pipe_6072.json?token=p.eyJ1IjogImJlMjI0MjJiLWMxZDMtNDM3OC05OTY3LTBiZmJlZTI4ZDM0ZSIsICJpZCI6ICIzMDUxYTc1OC1kNTk0LTQ3YTUtYjY3Mi1iNmFjYmI5Y2I0ZTgifQ.-8_l7AY-hvEKWkhGpJ29SYd1-SSGiiTWGcMTszsOrRI";
+    fetch(API_URL)
+        .then(response => response.json())
+        .then(data => {
+            const workExperienceList = document.getElementById('workExperienceList');
+            
+            // Sort the data array by the startDate field in descending order
+            data.data.sort((a, b) => new Date(b.startDate) - new Date(a.startDate));
+
+            data.data.forEach(item => {
+                const li = document.createElement('li');
+                li.innerHTML = `
+                    <strong>${item.Company}: ${item.Position}</strong><br>
+                    <strong>${item.Start_Date} – ${item.End_Date}</strong><br>
+                    <i>${item.Location}</i><br>
+                    ${item.Responsibilities.replace(/\n/g, '<br>')}
+                    <br>
+                `;
+                workExperienceList.appendChild(li);
+            });
+        });
+};
